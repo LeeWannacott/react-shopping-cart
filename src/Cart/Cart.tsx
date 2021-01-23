82cff673f06fb6d41aa54313cfe42d7e7f8fdbca
Lee Wannacott <wannacottl@gmail.com>
2021-01-23 15:15:17 +1300

    Uploading react shopping cart

--- src/Cart/Cart.tsx
+++ src/Cart/Cart.tsx
@@ -13,23 +13,22 @@ type Props = {
 };
 
 const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart }) => {
-const calculateTotal = (items: CartItemType[]) =>
-        items.reduce((ack: number, item) => ack + item.amount * item.price,0)
+    const calculateTotal = (items: CartItemType[]) =>
+        items.reduce((ack: number, item) => ack + item.amount * item.price, 0);
 
     return (
-
         <Wrapper>
             <h2> Your Shopping Cart </h2>
             {cartItems.length == 0 ? <p> No items in cart.</p> : null}
             {cartItems.map((item) => (
                 <CartItem
-                key={item.id}
-                item={item}
-                addToCart={addToCart}
-                removeFromCart={removeFromCart}
+                    key={item.id}
+                    item={item}
+                    addToCart={addToCart}
+                    removeFromCart={removeFromCart}
                 />
             ))}
-        <h2> Total: ${calculateTotal(cartItems).toFixed(2)} </h2>
+            <h2> Total: ${calculateTotal(cartItems).toFixed(2)} </h2>
         </Wrapper>
     );
 };
